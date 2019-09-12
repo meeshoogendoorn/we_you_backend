1986dc4de124f6f97853dca094921acf83fb1a6d
stefan de haan <stefan.de.haan@kpnmail.nl>
2019-09-12 09:51:00 +0200

    Added member realationship for company

--- backend/companies/models.py
+++ backend/companies/models.py
@@ -1,6 +1,14 @@
 from django.db.models import Model
 from django.db.models.fields import CharField
+from django.db.models.fields.related import CASCADE, OneToOneField, ForeignKey
+from accounts.models import User
 
 
 class Company(Model):
     name = CharField(max_length=255)
+
+
+class Member(Model):
+    """Connects an accounts.User to a company."""
+    company = OneToOneField(Company, on_delete=CASCADE)
+    account = ForeignKey(User, on_delete=CASCADE)
